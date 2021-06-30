c7f1dfb27044355b9ac33450f73449dfc6b06902
okdala2 <okdala2@gmail.com>
2021-06-29 18:23:56 -0700

    Added windowScene to window

---
+++ GHFollowers/ViewContoller.swift
@@ -0,0 +1,17 @@
+//
+//  ViewContoller.swift
+//  GHFollowers
+//
+//  Created by Dala  on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
