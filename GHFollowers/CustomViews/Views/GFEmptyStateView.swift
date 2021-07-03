6bcf175ac4e11218e0db4b367f7919a2d818014a
okdala2 <okdala2@gmail.com>
2021-07-03 11:56:13 -0700

    Added empty state view for when users  don't have nay followers

---
+++ GHFollowers/CustomViews/Views/GFEmptyStateView.swift
@@ -0,0 +1,55 @@
+//
+//  GFEmptyStateView.swift
+//  GHFollowers
+//
+//  Created by Dala  on 7/3/21.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+    
+    let messageLabel = GFTitleLabel(textAlignment: .center, fontsize: 17)
+    let logoImageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(message: String) {
+        super.init(frame: .zero)
+        
+        messageLabel.text = message
+        configure()
+    }
+    
+    
+    func configure() {
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+        
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .label
+        
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
+        ])
+    }
+}
