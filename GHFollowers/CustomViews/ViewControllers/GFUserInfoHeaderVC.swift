2fa2af67e65ed0d66beb60892b5bec7603ebd2b4
okdala2 <okdala2@gmail.com>
2021-07-03 16:04:10 -0700

    Added secondary Label

---
+++ GHFollowers/CustomViews/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,20 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollowers
+//
+//  Created by Dala  on 7/3/21.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+}
