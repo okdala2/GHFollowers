6fb5c5aa776be9425d632a8d17bb06e5c6229838
okdala2 <okdala2@gmail.com>
2021-07-04 07:23:06 -0700

    Added add or removed favorites through persistence

---
+++ GHFollowers/Manager/PersistanceManager.swift
@@ -0,0 +1,81 @@
+//
+//  PersistanceManager.swift
+//  GHFollowers
+//
+//  Created by Dala  on 7/4/21.
+//
+
+import Foundation
+
+enum PersistanceManager {
+    static private let defaults = UserDefaults.standard
+    
+    enum keys {
+        static let favorites = "favorites"
+    }
+    
+    static func updateWith(favorite: Follower, actionType: PersistanceActionType, completed: @escaping (GFError) -> Void ) {
+        retreiveFavorites { result in
+            switch result {
+            
+            case .success(let favorites):
+                var retreivedFavorites = favorites
+                
+                switch actionType {
+                
+                case .add:
+                    guard !retreivedFavorites.contains(favorite) else {
+                        completed(.alreadyinFavorites)
+                        return
+                    }
+                    
+                    retreivedFavorites.append(favorite)
+                    break
+                    
+                case .remove:
+                    retreivedFavorites.removeAll { $0.login == favorite.login }
+                    break
+                }
+                
+                completed(save(favorites: retreivedFavorites)!)
+               
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
+    //Anytime you needto save custom data to persist it needs to be encodable / decodable
+    static func retreiveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        
+        guard let favoritesData = defaults.object(forKey: keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFavortie))
+        }
+    }
+    
+    static func save(favorites: [Follower]) -> GFError? {
+        
+        do {
+            let encoder = JSONEncoder()
+            let enconderFavorites = try encoder.encode(favorites)
+            defaults.setValue(enconderFavorites, forKey: keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavortie
+        }
+    }
+}
+
+enum PersistanceActionType {
+    case add
+    case remove
+}
--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -13,4 +13,6 @@ enum GFError: String, Error {
     case invalidResponse = "Invalid response from the server, please try again"
     case invalidData = "The data received from the server was invalid please try again"
     
+    case unableToFavortie = "There was an error favoriting this user, please try again"
+    case alreadyinFavorites = "You've already saved this user"
 }
