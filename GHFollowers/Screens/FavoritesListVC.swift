2d52fc980a4a107e81220387d353df0cad6c1772
okdala2 <okdala2@gmail.com>
2021-07-05 11:56:48 -0700

    Added Swipe to Delete to Favorited table view

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -12,16 +12,15 @@ class FavoritesListVC: UIViewController {
     let tableView = UITableView()
     var favorites: [Follower] = []
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        getFavorites()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureViewController()
         configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         getFavorites()
     }
     
@@ -35,7 +34,9 @@ class FavoritesListVC: UIViewController {
     
     func configureTableView() {
         view.addSubview(tableView)
+        
         tableView.frame = view.bounds
+        
         tableView.rowHeight = 80
         tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.resuseId)
         
@@ -83,5 +84,31 @@ extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
         cell.set(favorite: favortie)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        
+        let destVC = FollowerListVC()
+        destVC.username = favorite.login
+        destVC.title = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else {  return  }
+            
+            self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
     }
 }
