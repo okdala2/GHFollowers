c0c867574362eb4561967895637d227caabc1e9a
okdala2 <okdala2@gmail.com>
2021-07-17 14:30:40 -0700

    switched height with width anchor on logo image

--- GHFollowers/CustomViews/Views/GFEmptyStateView.swift
+++ GHFollowers/CustomViews/Views/GFEmptyStateView.swift
@@ -45,7 +45,7 @@ class GFEmptyStateView: UIView {
             messageLabel.heightAnchor.constraint(equalToConstant: 200),
             
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
-            logoImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
             logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
         ])
